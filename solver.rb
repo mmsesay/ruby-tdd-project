e9c69c258b444696bd1a373785cd90e25ef9be17
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-12-23 20:30:59 +0200

    Write the factorial code Related to the factorial Test-driven development

--- solver.rb
+++ solver.rb
@@ -1,1 +1,11 @@
-
+class Solver
+  def factorial(arg)
+    if arg.zero?
+      1
+    elsif arg.positive?
+      arg * factorial(arg - 1)
+    else
+      'Please enter a positive number'
+    end
+  end
+end
