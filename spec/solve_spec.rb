1c9f02d617f9d547c655937e4203743b7437c697
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-12-23 20:22:51 +0200

    Add factorial testing

--- spec/solve_spec.rb
+++ spec/solve_spec.rb
@@ -17,29 +17,5 @@ describe Solver do
     it 'Check if the factorial arg is negative' do
       expect(@solver.factorial(-3)).to eq 'Please enter a positive number'
     end
-
-    it 'Check if the value is reverse' do
-      expect(@solver.reverse('value')).to eq 'eulav'
-    end
-
-    it 'Check if the value is not reverse' do
-      expect(@solver.reverse(3)).to eq 'Please enter a string'
-    end
-
-    it 'Check if the value is fizzbuzz' do
-      expect(@solver.fizzbuzz(15)).to eq 'fizzbuzz'
-    end
-
-    it 'Check if the value is fizz' do
-      expect(@solver.fizzbuzz(3)).to eq 'fizz'
-    end
-
-    it 'Check if the value is buzz' do
-      expect(@solver.fizzbuzz(5)).to eq 'buzz'
-    end
-
-    it 'Check if the value is not divisible' do
-      expect(@solver.fizzbuzz(7)).to eq '7'
-    end
   end
 end
